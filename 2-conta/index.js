73fece2b1527167add39f9cec7328c95ecaeb14a
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-09-12 15:33:27 -0300

    Viewing updated account information

--- 2-conta/index.js
+++ 2-conta/index.js
@@ -4,6 +4,18 @@ const minhaConta = new Conta(12345);
 
 minhaConta.info();
 
+minhaConta.saldo += 500;
+console.log('Depósito de 500 realizado.');
+
+// Exibindo informações atualizadas da conta
+minhaConta.info();
+
+// Simulando um saque
+minhaConta.saldo -= 200;
+console.log('Saque de 200 realizado.');
+
+// Exibindo informações atualizadas da conta
+minhaConta.info();
 
 // imports
 
